6fa7e327eea2b61a77776538dd65e508789fa7c5
rajosch <raphaeljschmid@protonmail.com>
2022-12-02 22:00:55 -0600

    added server/api

--- components/layout/navbar.vue
+++ components/layout/navbar.vue
@@ -5,18 +5,13 @@
 
 
 <template>
-    <div class="navbar relative">
-        hello
+    <div class="relative">
         <div class="max-w-7xl mx-auto px-4 sm:px-6">
-            hello
-            <div class="flex justify-between border-b-2 border-gray-100">
-                hello
-                <div class="flex justify-between min-w-full md:min-w-0">
-                    hello
-                    <div class="md:hidden mt-4 bg-black">
-                        Hello
-                    </div>
-                </div>
+            <button @click="registerWithEmail('raphael', 'raphael', 'raphael', '123admin')">
+                Sign Up
+            </button>
+            <div>
+                {{user}}
             </div>
         </div>
     </div>
